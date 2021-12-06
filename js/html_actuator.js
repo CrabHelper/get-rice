ded6c43e231067e62e6ba7324e1ec921aefd576c
CrabHelper <91166765+CrabHelper@users.noreply.github.com>
2021-12-06 14:30:59 -0500

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "You wasted rice!";
+  var message = won ? "Not bad..." : "You wasted rice!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
